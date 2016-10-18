70a4253feeafd09d71e78c0e81906302c80d7db5
gnkatchmar <gunk55@msn.com>
2016-10-18 15:25:03 -0700

    password addition working and passing tests

--- test/greeting-test.js
+++ test/greeting-test.js
@@ -1,17 +1,22 @@
-var assert = require('assert');
+var assert = require('chai').assert;
 var greet = require('../lib/greeting');
+var greetName = '401 Student!';
 
 describe('greeting', function() {
-  it('greeting === "Welcome to Node, 401 Student!', function() {
-    var greeting = greet('401 Student!');
 
-    assert.equal(greeting, 'Welcome to Node, 401 Student!');
+  var greeting = greet(greetName);
+  // update '401 Student!' if greetName is changed
+  var regex = /^Welcome to Node, 401 Student! Your temporary password is: /;
+
+  it('starts with welcome phrase, greetName, password phrase', function() {
+    // tests the beginning of the greeting
+    assert.match(greeting, regex);
   });
-});
 
-describe('greeting length', function() {
+  //Expected length of entire greeting based on '401 Student!'
   it('greeting length', function() {
-    var greetingLength = greet('401 Student!').length;
-    assert.equal(greetingLength, 29);
+    var greetingLength = greet(greetName).length;
+    assert.equal(greetingLength, 66);
   });
+
 });
